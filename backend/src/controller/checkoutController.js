55a1d0c09f2223cb54e57d057ad5548c55dc4931
rayhan marcello ananda purnomo <rayhanmarcello222@gmail.com>
2026-04-07 23:14:43 +0800

    create controller for checkout service

---
+++ backend/src/controller/checkoutController.js
@@ -0,0 +1,27 @@
+const { checkoutSchema } = require("../validations/checkoutValidation");
+const checkoutService = require("../services/checkoutService");
+
+const checkout = async (req, res, next) => {
+  try {
+    const { error, value } = checkoutSchema.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({
+        message: error.details[0].message,
+      });
+    }
+
+    const result = await checkoutService.checkout(value);
+
+    return res.status(201).json({
+      message: "Checkout berhasil",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  checkout,
+};
